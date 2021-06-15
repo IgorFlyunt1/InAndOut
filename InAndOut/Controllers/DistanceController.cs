23da124da9eb5ffdb616f08bc195abacd4d5e8f3
igorflyunt <igorflyunt2@gmail.com>
2021-06-15 22:12:59 +0300

    Created new controller + view  - Distance and route between two point on Google Maps

---
+++ InAndOut/Controllers/DistanceController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class DistanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult SimpleDistance()
+        {
+            return View();
+        }
+
+        public IActionResult MultipleDistance()
+        {
+            return View();
+        }
+
+
+    }
+}
